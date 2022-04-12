cb2f8af0eb24c98c74ef6f06f64fe5d91a25bfdc
spatel <sappystar27@gmail.com>
2022-04-12 23:49:44 +0100

    Updated JS

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//profile information
+const overview = document.querySelector (".overview");
+const username = "bluebird5-star";
+
+const getInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+
+const data = await userInfo.json();
+displayInfo(data);
+};
+
+getInfo();
+
+
+
+
+const displayInfo = function (data){
+const div = document.createElement("user-info");
+div.classList.add("user-info");
+
+div.innerHTML =`
+<figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+    `;
+overview.append(div);
+};
